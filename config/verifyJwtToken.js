6e3072c7003572a7b01b4a941d156431d407655a
NiloyNs <niloysarkar1998@gmail.com>
2024-06-06 16:21:24 +0530

    add bearer

--- config/verifyJwtToken.js
+++ config/verifyJwtToken.js
@@ -2,18 +2,21 @@ const jwt = require('jsonwebtoken'); // Import jsonwebtoken for token verificati
 
 // Middleware function to verify JWT token
 module.exports.verifyToken = (req, res, next) => {
-    const token = req.header('Authorization'); // Get the token from the request headers
-
-    if (!token) {
-        return res.status(401).json({ message: 'Access Denied. No token provided.' }); // Return error if no token is provided
+    const authHeader = req.header('Authorization');
+    
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Access Denied. Invalid or no token provided.' });
     }
+
+    const token = authHeader.split(' ')[1]; // Extract the token part without the "Bearer" prefix
 
     try {
-        const verified = jwt.verify(token, process.env.JWT_SECRET); // Verify the token using JWT_SECRET from environment variables
-        req.user = verified; // If token is valid, set `req.user` to the decoded token payload
-        console.log(req.user);
-        next(); // Call next middleware function
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verified;
+        next();
     } catch (error) {
-        res.status(400).json({ message: 'Invalid Token' }); // Return error if token is invalid
+        res.status(400).json({ message: 'Invalid Token' });
     }
 };
+
+module.exports
