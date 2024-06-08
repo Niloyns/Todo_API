8adf0a1aa52468f0377fa02a26dbc023a2fd70c7
NiloyNs <niloysarkar1998@gmail.com>
2024-06-08 12:51:26 +0530

    add env MONGO_URL

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose"); // Import mongoose module
+const dotenv = require("dotenv"); // Import dotenv module to load environment variables
 
-// Connect to the MongoDB database
-mongoose.connect("mongodb://localhost/todosApi")
+dotenv.config(); // Load environment variables from .env file
+
+// Connect to MongoDB using the MONGO_URL environment variable
+mongoose.connect(process.env.MONGO_URL)
     .then(() => {
         console.log("mongodb connection success"); // Log success message if connection is successful
     })
     .catch((err) => {
-        console.log(err); // Log error message if connection fails
+        console.error("mongodb connection error:", err); // Log error message if connection fails
     });
 
-module.exports = mongoose; // Export the mongoose instance
+module.exports = mongoose; // Export the mongoose instance for use in other parts of the application
